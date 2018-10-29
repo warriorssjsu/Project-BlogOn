ecc9fe1ffff842dfadbeb488d5c1928e5598c9d2
poojaagrwal <poojaagrwl@users.noreply.github.com>
2018-10-29 16:14:13 -0700

    committing docker related changes

---
+++ BlogOnProject/src/test/java/com/warriors/blogOnProject/SecurityConfigurationTest.java
@@ -0,0 +1,74 @@
+package com.warriors.blogOnProject;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.security.extensions.saml2.config.SAMLConfigurer.saml;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@ContextConfiguration(classes = SecurityConfiguration.class)
+public class SecurityConfigurationTest {
+	@Autowired
+	WebApplicationContext wac;
+
+	MockMvc mockMvc;
+
+	@Before
+	public void setup() {
+		mockMvc = MockMvcBuilders
+				.webAppContextSetup(wac)
+				.apply(springSecurity())
+				.build();
+	}
+
+	@Test
+	public void protectedUrlRedirectsToDiscovery() throws Exception {
+		mockMvc.perform(get("/protected/"))
+			.andExpect(status().is3xxRedirection())
+			.andExpect(redirectedUrl("https://localhost:8443/saml/discovery?entityID=https%3A%2F%2Flocalhost%3A8443%2Fsaml%2Fmetadata&returnIDParam=idp"));
+	}
+
+	@Test
+	public void discoveryRedirectsLogin() throws Exception {
+		mockMvc.perform(get("/saml/discovery").param("entityID","https://localhost:8443/saml/metadata").param("returnIDParam","idp"))
+			.andExpect(status().is3xxRedirection())
+			.andExpect(redirectedUrl("https://localhost:8443/saml/login?disco=true&idp=http%3A%2F%2Fwww.okta.com%2Fexk5id72igJRNtH5M0h7"));
+	}
+
+	@Test
+	public void loginRendersSAMLRequest() throws Exception {
+		mockMvc.perform(get("/saml/login").param("disco", "true").param("idp","http://www.okta.com/exkh09sxlvM5Ujviu0h7"))
+			.andExpect(status().isOk())
+			.andExpect(content().string(containsString("<input type=\"hidden\" name=\"SAMLRequest\" value=\"")));
+
+	}
+
+	@Test
+	public void renderMetadata() throws Exception {
+		mockMvc.perform(get("/saml/metadata"))
+				.andExpect(status().isOk())
+				.andExpect(content().string(containsString("<md:EntityDescriptor xmlns:md=\"urn:oasis:names:tc:SAML:2.0:metadata\"")));
+	}
+
+}
